00aac9b3b6896f4a2ca95697f93d37942c8f6d6f
Francesco <francesco@efem.dev>
2024-05-12 17:36:21 +0200

    fix date validation

--- src/components/LandingForm.tsx
+++ src/components/LandingForm.tsx
@@ -145,6 +145,7 @@ const LandingForm = ({ onLandingDataFiled, initialData }: LandingFormProps) => {
                 </svg>
               </div>
               <input
+                max={new Date().toISOString().split('T')[0]}
                 type="date"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 {...register('date')}
