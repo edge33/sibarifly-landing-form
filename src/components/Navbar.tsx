9fff50162bf7c885dafcb30f2f13f7ec9b406b74
Francesco <francesco@efem.dev>
2024-05-12 17:26:05 +0200

    db connection

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,8 +56,8 @@ const Navbar = () => {
           className={`${!isMenuVisible && 'hidden'} w-full flex flex-col md:w-auto md:flex md:flex-row md:items-center`}
           id="navbar-default"
         >
-          {/* <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            {/* <li>
               <a
                 href="#"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
@@ -65,13 +65,13 @@ const Navbar = () => {
               >
                 Home
               </a>
-            </li>
+            </li> */}
             <li>
               <a
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                About
+                Atterraggio
               </a>
             </li>
             <li>
@@ -79,10 +79,10 @@ const Navbar = () => {
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Services
+                Decollo
               </a>
             </li>
-            <li>
+            {/* <li>
               <a
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -97,8 +97,8 @@ const Navbar = () => {
               >
                 Contact
               </a>
-            </li>
-          </ul> */}
+            </li> */}
+          </ul>
           <button
             id="theme-toggle"
             type="button"
