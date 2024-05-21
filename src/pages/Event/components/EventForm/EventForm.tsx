fe8624d978c8543e5c87420286a9b9f7f31ebcb5
Francesco <francesco@efem.dev>
2024-05-22 00:48:32 +0200

    new event schema

--- src/pages/Event/components/EventForm/EventForm.tsx
+++ src/pages/Event/components/EventForm/EventForm.tsx
@@ -465,6 +465,26 @@ const LandingForm = ({ onLandingDataFiled, initialData }: LandingFormProps) => {
 
           {/* <div className="w-full">
             <label
+              htmlFor="mobilePhone"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Numero di telefono
+            </label>
+            <input
+              type="tel"
+              id="mobilePhone"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              {...register('mobilePhone')}
+            />
+            {errors?.mobilePhone && (
+              <p className="pt-2.5 text-red-500">
+                {errors.mobilePhone.message}
+              </p>
+            )}
+          </div>
+
+          {/* <div className="w-full">
+            <label
               htmlFor="destination"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
