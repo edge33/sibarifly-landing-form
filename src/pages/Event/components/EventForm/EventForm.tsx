21d646cad012146f43336e8880152ad419cff72a
Francesco Maida <francescogithub@efem.dev>
2024-11-13 12:18:31 +0100

    fix(dates) removed guard for present date (#11)

--- src/pages/Event/components/EventForm/EventForm.tsx
+++ src/pages/Event/components/EventForm/EventForm.tsx
@@ -142,7 +142,7 @@ const LandingForm = ({ onLandingDataFiled, initialData }: LandingFormProps) => {
                 </svg>
               </div>
               <input
-                max={getCurrentDateTime()}
+                // max={getCurrentDateTime()}
                 type="datetime-local"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 {...register('arrivalDateTime')}
